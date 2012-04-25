f115f863f9be037d7ff0cbfc1ca1229778724b78
Honza Synek <jan.synek@fsv.cvut.cz>
2012-04-26 00:11:32 +0200

    Adjustment page updated

--- src/main/java/cz/cvut/fsv/webgama/controller/AdjustmentController.java
+++ src/main/java/cz/cvut/fsv/webgama/controller/AdjustmentController.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-import cz.cvut.fsv.webgama.domain.User;
-import cz.cvut.fsv.webgama.service.UserManager;
-
 @Controller
 public class AdjustmentController extends AbstractController {
 
-	@Autowired
-	private UserManager userManager;
-	
 	@DateTimeFormat
 	private Date date;
 
@@ -33,25 +26,12 @@ public class AdjustmentController extends AbstractController {
 		long startTime = System.nanoTime();
 
 		ModelAndView mav = new ModelAndView("/adjustment/adjustment");
-		
-		User user = userManager.getUser(5);
 
-		boolean perm = request.isUserInRole("ROLE_ADMIN");
-
-		if (perm) {
-			System.out.println("ANO");
-		} else {
-			System.out.println("NE");
-		}
-
-		String firstName = user.getFirstName();
-		String lastName = user.getLastName();
+		request.isUserInRole("ROLE_ADMIN");
 		
 		date = new Date();
 		mav.addObject("date", date);
 
-		mav.addObject("firstName", firstName);
-		mav.addObject("lastName", lastName);
 		mav.addObject("admin", request.isUserInRole("ROLE_ADMIN"));
 
 		double time = (double) (System.nanoTime() - startTime) / 1000000;
