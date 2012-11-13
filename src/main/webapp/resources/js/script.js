e2d84671af8af46096868502d030e0a20378f99d
Jan Synek <jan.synek@fsv.cvut.cz>
2012-11-13 17:18:23 +0100

    header design updated, dropdown menus for locales and account

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
 
 	// DROPDOWN MENU behaviour
-	$('.active_link').click(function() {
+	$('a#account').click(function() {
+		$('#locale_dropdown').slideUp(200);
 		$('#account_dropdown').slideToggle(200);
+		return false;
+	});
+	
+	$('a#locales').click(function() {
+		$('#account_dropdown').slideUp(200);
+		$('#locale_dropdown').slideToggle(200);
 		return false;
 	});
 
 	$('#account_dropdown').click(function(e) { //not hide when click on dropdown menu
 		e.stopPropagation();
 	});
+	
+	$('#locale_dropdown').click(function(e) { //not hide when click on dropdown menu
+		e.stopPropagation();
+	});
 
 	$(document).click(function() {
 		$('#account_dropdown').slideUp(200);
+		$('#locale_dropdown').slideUp(200);
 	});
 });
