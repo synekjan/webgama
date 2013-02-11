1e57365a721baa498de250ce59be68657b675e6b
Jan Synek <jan.synek@fsv.cvut.cz>
2013-02-11 22:19:24 +0100

    admin section styled

--- src/main/java/cz/cvut/fsv/webgama/controller/AdminController.java
+++ src/main/java/cz/cvut/fsv/webgama/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import cz.cvut.fsv.webgama.domain.User;
 import cz.cvut.fsv.webgama.service.UserManager;
@@ -20,14 +21,23 @@ public class AdminController {
 	@Inject
 	UserManager userManager;
 
-	@RequestMapping(value = "", method = RequestMethod.GET)
-	public String getUserList(HttpServletRequest request, Model model) {
+	@RequestMapping(value = {"", "/users"}, method = RequestMethod.GET)
+	public ModelAndView showUsers(HttpServletRequest request, Model model) {
 
+		ModelAndView mav = new ModelAndView("/admin/users/users");
 		List<User> list = userManager.getUserList();
 
-		model.addAttribute("userList", list);
+		mav.addObject("userList", list);
 
-		return "/admin/userlist";
+		return mav;
+	}
+	
+	@RequestMapping(value = "/users/delete", method = RequestMethod.GET)
+	public ModelAndView deleteUsers(HttpServletRequest request, Model model) {
+
+		ModelAndView mav = new ModelAndView("/admin/users/delete");
+
+		return mav;
 	}
 
 }
