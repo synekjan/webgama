9c2c92166c6daddd2b47113173483cc28187843d
Jan Synek <jan.synek@fsv.cvut.cz>
2013-03-18 15:39:56 +0100

    OutputDao implemented

--- src/main/java/cz/cvut/fsv/webgama/dao/OutputDao.java
+++ src/main/java/cz/cvut/fsv/webgama/dao/OutputDao.java
@@ -4,7 +4,13 @@ import cz.cvut.fsv.webgama.domain.Calculation;
 import cz.cvut.fsv.webgama.domain.Output;
 
 public interface OutputDao {
-	
+
+	public void insert(Output output, Long calculationId);
+
+	public void delete(Output output);
+
+	public void update(Output output);
+
 	public Output findOutputInCalculation(Calculation calculation);
 
 }
--- src/main/java/cz/cvut/fsv/webgama/dao/jdbc/JdbcOutputDao.java
+++ src/main/java/cz/cvut/fsv/webgama/dao/jdbc/JdbcOutputDao.java
@@ -1,5 +1,10 @@
 package cz.cvut.fsv.webgama.dao.jdbc;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.joda.time.DateTime;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import cz.cvut.fsv.webgama.dao.OutputDao;
@@ -9,9 +14,61 @@ import cz.cvut.fsv.webgama.domain.Output;
 public class JdbcOutputDao extends JdbcDaoSupport implements OutputDao {
 
 	@Override
-	public Output findOutputInCalculation(Calculation calculation) {
-		// TODO Auto-generated method stub
-		return null;
+	public void insert(Output output, Long calculationId) {
+		String sql = "INSERT INTO outputs (calculation_id, xml_content, text_content, html_content, svg_content) VALUES (?, ?, ?, ?, ?)";
+
+		getJdbcTemplate().update(
+				sql,
+				new Object[] { calculationId, output.getXmlContent(), output.getTextContent(), output.getHtmlContent(),
+						output.getSvgContent() });
+
 	}
 
+	@Override
+	public void delete(Output output) {
+
+		String sql = "DELETE FROM outputs WHERE output_id = ?";
+
+		getJdbcTemplate().update(sql, new Object[] { output.getId() });
+
+	}
+
+	@Override
+	public void update(Output output) {
+
+		String sql = "UPDATE outputs SET xml_content=?, text_content=?, html_content=?, svg_content=?, time=? WHERE output_id=?";
+
+		getJdbcTemplate().update(
+				sql,
+				new Object[] { output.getXmlContent(), output.getTextContent(), output.getHtmlContent(),
+						output.getSvgContent(), output.getTime(), output.getId() });
+	}
+
+	@Override
+	public Output findOutputInCalculation(Calculation calculation) {
+
+		String sql = "SELECT * FROM outputs WHERE calculation_id =?";
+
+		Output output = getJdbcTemplate().queryForObject(sql, new Object[] { calculation.getId() }, new OutputMapper());
+
+		return output;
+	}
+
+	private class OutputMapper implements RowMapper<Output> {
+
+		@Override
+		public Output mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+			Output output = new Output();
+
+			output.setId(rs.getLong("output_id"));
+			output.setXmlContent(rs.getString("xml_content"));
+			output.setTextContent(rs.getString("text_content"));
+			output.setHtmlContent(rs.getString("html_content"));
+			output.setSvgContent(rs.getString("svg_content"));
+			output.setTime(new DateTime(rs.getTimestamp("time").getTime()));
+
+			return output;
+		}
+	}
 }
